ba86ce86cbd8e967992d3567437b501dfc79fa65
rdgoite <rdgoite@gmail.com>
2019-06-20 10:04:55 +0100

    Initiliased demo Mockk project.

---
+++ src/main/kotlin/io/github/rdgoite/demomockk/DemoMockkApplication.kt
@@ -0,0 +1,11 @@
+package io.github.rdgoite.demomockk
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DemoMockkApplication
+
+fun main(args: Array<String>) {
+	runApplication<DemoMockkApplication>(*args)
+}
---
+++ src/test/kotlin/io/github/rdgoite/demomockk/DemoMockkApplicationTests.kt
@@ -0,0 +1,16 @@
+package io.github.rdgoite.demomockk
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class DemoMockkApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
