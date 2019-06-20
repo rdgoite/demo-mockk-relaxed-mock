a5509def892995694b5a700ac5214124064e35b2
rdgoite <rdgoite@gmail.com>
2019-06-20 12:40:57 +0100

    Directly instantiating instances for test results in the same issue.

--- src/test/kotlin/io/github/rdgoite/demomockk/SpringRepositoryTest.kt
+++ src/test/kotlin/io/github/rdgoite/demomockk/SpringRepositoryTest.kt
@@ -2,26 +2,32 @@ package io.github.rdgoite.demomockk
 
 import com.ninjasquad.springmockk.MockkBean
 import io.mockk.every
+import io.mockk.mockk
 import io.mockk.verify
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.junit.jupiter.SpringExtension
 
-@ExtendWith(SpringExtension::class)
-@SpringBootTest(classes = [DefaultBookService::class])
-class DemoServiceTest(@Autowired private val bookService: BookService) {
+class DemoServiceTest {
 
-    @MockkBean
+    private lateinit var bookService: BookService
+
     private lateinit var bookRepository: BookRepository
+
+    @BeforeEach
+    fun setUp() {
+        bookRepository = mockk(relaxed = true)
+        bookService = DefaultBookService(bookRepository)
+    }
 
     @Test
     fun `Demonstrate issue with relaxed Mockks`() {
         // given:
         val book = Book(title = "Testing with Mockk")
-        every { bookRepository.save(any<Book>()) } returns Book(title = "test")
 
         // when:
         bookService.addToLibrary(book)
